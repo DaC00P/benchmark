12deb8a50f57f6b5fcec05e7fe09cc88e65f2297
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-07-22 11:46:23 -0700

    first demo sort functional

--- react/components/control_panel.jsx
+++ react/components/control_panel.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as ClientActions from '../actions/client_actions';
+const Library = require('../../sorts');
 
 const ControlPanel = React.createClass({
 
   getInitialState(){
-    return {selected: 1};
+    return {selected: '1'};
   },
 
   selectCheck(n){
@@ -15,17 +16,30 @@ const ControlPanel = React.createClass({
     this.setState({selected: evt.target.id});
   },
 
+  demoSort(evt){
+    let pane = document.getElementById(`code-${this.state.selected}`);
+    let text = Library[evt.target.id].toString();
+    pane.value = text;
+  },
+
   render(){
     return(
       <div className="control-panel-container">
         <h4>CONTROL PANEL</h4>
-        <button className='pane-selector' id='1'
-                disabled={this.selectCheck(1)}
-                onClick={this.selectPane}>1</button>
-              <button className='pane-selector' id='2'
-                disabled={this.selectCheck(2)}
-                onClick={this.selectPane}>2</button>
+        <button
+          className='pane-selector' id='1'
+          disabled={this.selectCheck('1')}
+          onClick={this.selectPane}>1</button>
+        <button
+          className='pane-selector' id='2'
+          disabled={this.selectCheck('2')}
+          onClick={this.selectPane}>2</button>
+        <button
+          className='demo-sort' id='bubbleSort'
+          onClick={this.demoSort}>Bubble Sort</button>
+
       </div>
+
     );
   }
 
