1add8187e017f8481f3ae4e495566b7f5c7d42ee
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-07-21 17:52:46 -0700

    data scaling on back end added

--- controller.js
+++ controller.js
@@ -13,27 +13,33 @@ module.exports = {
     testN.forEach( (n) => {
       let res1 = SortUtil.benchmark(SortUtil.bubbleSort, n, 10);
       let res2 = SortUtil.benchmark(SortUtil.quickSort, n, 10);
-      results1.push({x: n/10 - 50, y: res1});
-      results2.push({x: n/10 - 50, y: res2});
+      results1.push({x: n/100 - 50, y: res1});
+      results2.push({x: n/100 - 50, y: res2});
     });
     // console.log(codeObj.algos.method1);
     // console.log(codeObj.algos.method2);
     // // console.log(method1, method2);
     // console.log(method1(arr));
     // console.log(method2(arr));
+
     console.log(results1);
     console.log(results2);
-    const data1 = this.transform(results1);
-    const data2 = this.transform(results2);
+    let max = results1.concat(results2).map( res => res.y).sort( (a, b) => a - b ).pop();
+    console.log(max);
+    this.transform(results1, max);
+    this.transform(results2, max);
+
+    console.log(results1);
+    console.log(results2);
 
     return {data1: results1, data2: results2};
 
   },
 
-  transform(data){
+  transform(data, max){
     data.forEach( (point, idx) => {
-      point.x = (600 / data.length + 1) * (idx + 1);
-      point.y *= (600 / data.slice(-1).y);
+      point.x = (600 / (data.length + 1)) * (idx + 1);
+      point.y *= (590 / max);
     });
   },
 
