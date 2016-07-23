9489450bf0f446503299870e58890b5eb74b0ad9
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-22 20:07:22 -0700

    working mostly-secure VM that executes a recursive quicksort and is ready to accept your codez <3

--- controller.js
+++ controller.js
@@ -4,7 +4,8 @@ const SortUtil2 = require('./sorts0');
 const arrayToSort = SortUtil.make(1000);
 
 const vm  = require('vm');
-
+const {VM} = require('vm2');
+const jailed = require('jailed');
 
 
 module.exports = {
@@ -52,33 +53,71 @@ module.exports = {
   receiveTestCode(codeObj) {
     let method1 = codeObj.algos.method1;
     let method2 = codeObj.algos.method2;
-    this.bootVM();
+    this.bootVM(method1, method2);
   },
 
 
-  bootVM() {
+  bootVM(m1, m2) {
     console.log('in the vm booter');
-    const script = new vm.Script("var arr = [1, 2, 3]; for (var i = 0; i < arr.length; i++) {console.log(arr[i]);};", {
-      filename: 'my-index.js', // filename for stack traces
-      lineOffset: 1, // line number offset to be used for stack traces
-      columnOffset: 1, // column number offset to be used for stack traces
-      displayErrors: true,
-      timeout: 1000 // ms
+    console.log(m1);
+    console.log(m2);
+
+
+    const worker = new VM({
+        timeout: 1000,
+        sandbox: {console}
     });
-    //put the code to benchmark in the sandbox, then run it in line 79
-    const sandbox = vm.createContext({console});
 
-    console.log('vm start');
+    worker.run(`
+      var arr1 = [5, 4, 3, 2, 1]
+      var qs = function quickSort(arr) {
+        if (arr.length <= 1) {
+          return arr;
+        }
+
+        var pivot = arr[0];
+        var left = [];
+        var right = [];
+
+        for (var i = 1; i < arr.length; i++) {
+          if (arr[i] < pivot) {
+            left.push(arr[i]);
+          } else {
+            right.push(arr[i]);
+          }
+        }
+
+        return this.qs(left).concat([pivot]).concat(this.qs(right));
+      };console.log(qs);
+
+      console.log(qs(arr1));
+          `);
 
 
-    try {
-      //this is where the script will get called  to exec
-      script.runInContext(sandbox, {timeout: 5000, displayErrors: true, });
-    }
-    catch (e) {
-      // Exception thrown after 1000ms
-      console.log(e);
-    }
+    // "var arr = [1, 2, 3]; for (var i = 0; i < arr.length; i++) {console.log(arr[i]);};"
+    // const script = new vm.Script("let i = 0; while (true) {console.log(i); i++;};", {
+    //   filename: 'my-index.js', // filename for stack traces
+    //   lineOffset: 1, // line number offset to be used for stack traces
+    //   columnOffset: 1, // column number offset to be used for stack traces
+    //   displayErrors: true,
+    //   timeout: 500 // ms
+    // });
+    // //put the code to benchmark in the sandbox, then run it in line 79
+    // const sandbox = vm.createContext({console});
+    //
+    //
+    //
+    // console.log('vm start');
+    //
+    //
+    // try {
+    //   //this is where the script will get called  to exec
+    //   script.runInContext(sandbox, {timeout: 1000, displayErrors: true, });
+    // }
+    // catch (e) {
+    //   // Exception thrown after 1000ms
+    //   console.log(e);
+    // }
     console.log('vm end');
 
   }
