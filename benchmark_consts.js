0e85ee9664167fd5d9bfd19d5c46a7a95ebad3e7
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-24 15:17:53 -0700

    array maker integrated into vm

--- benchmark_consts.js
+++ benchmark_consts.js
@@ -1,25 +1,40 @@
-const SearchConstants = require('./search_consts');
+// const SearchConstants = require('./search_consts');
+
+const testArrayMaker =
+  `
+    var maker = function make(n){
+      let arr = [];
+      for (let i = 0; i < n; i++) {
+        let rand = Math.floor(Math.random() * 1000000);
+        arr.push(rand);
+      }
+      return arr;
+    };
+  `;
+
+const benchMarker =
+  `
+    var bm = function benchmark(sortFunc, length, iterations){
+      let arr = this.make(length);
+      let t = 0;
+      for (let i = 0; i < iterations; i++) {
+        let a = arr.slice(0);
+        let t0 = new Date();
+        sortFunc(a);
+        let t1 = new Date();
+        t += (t1 - t0);
+      }
+      return t;
+    };
+
+  `;
 
 module.exports = {
-  make(n){
-    let arr = [];
-    for (let i = 0; i < n; i++) {
-      let rand = Math.floor(Math.random() * 1000000);
-      arr.push(rand);
-    }
-    return arr;
+  getArrayMaker() {
+    return testArrayMaker;
   },
 
-  benchmark(sortFunc, length, iterations){
-    let arr = this.make(length);
-    let t = 0;
-    for (let i = 0; i < iterations; i++) {
-      let a = arr.slice(0);
-      let t0 = new Date();
-      sortFunc(a);
-      let t1 = new Date();
-      t += (t1 - t0);
-    }
-    return t;
+  getBenchMarker() {
+    return benchMarker;
   }
 };
