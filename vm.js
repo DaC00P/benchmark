ca5c49abfc24d565f85f49733e1a706a47413f4c
Daniel Cuperman <dacooperfish@gmail.com>
2016-07-26 11:00:43 -0700

    back end testing 2

--- vm.js
+++ vm.js
@@ -4,8 +4,6 @@ const SearchConstants = require('./search_consts');
 module.exports = {
     bootVM(method) {
      console.log('in the vm booter');
-     //modify to use m1, m2 when front end talks right
-
 
      let worker1 = new VM({
          timeout: 10000,
