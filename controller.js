b07b7a3b8a267da1772f53b367e721bc8c6268b3
DaC00P <dacuperman@gmail.com>
2016-07-26 10:24:30 -0700

    back end testing 1

--- controller.js
+++ controller.js
@@ -11,28 +11,32 @@ const arrayToSort = SortUtil.make(1000);
 
 module.exports = {
   receiveCode(codeObj) {
-    // let method1 = eval(codeObj.algos.method1);
-    // let method2 = eval(codeObj.algos.method2);
-    // // let testN = [1000, 2000, 3000, 4000, 5000, 6000, 7000];
-    // let testN = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-    // let results1 = [];
-    // let results2 = [];
-    // testN.forEach( (n) => {
-    //   let res1 = SortUtil.benchmark(method1, n, 1000);
-    //   let res2 = SortUtil.benchmark(method2, n, 1000);
-    //   results1.push({x: n, y: res1});
-    //   results2.push({x: n, y: res2});
-    // });
+    let method1 = codeObj.algos.method1;
+    let method2 = codeObj.algos.method2;
+    let lengthArr = codeObj.algos.lengthArr;
+    this.testCode(method1, method2, lengthArr);
+
     //
-    // let max = results1.concat(results2).map( res => res.y).sort( (a, b) => a - b ).pop();
-    // console.log(max);
-    // this.transform(results1, max);
-    // this.transform(results2, max);
 
-    this.receiveTestCode(codeObj);
+  },
 
-    // return {data1: results1, data2: results2};
+  testCode(method1, method2, lengthArr) {
+    let results1 = [];
+    let results2 = [];
 
+    lengthArr.forEach( (n) => {
+      let res1 = VM.bootVM(method1);
+      let res2 = VM.bootVM(method2);
+      results1.push({x: n, y: res1});
+      results2.push({x: n, y: res2});
+    });
+
+    let max = results1.concat(results2).map( res => res.y).sort( (a, b) => a - b ).pop();
+
+    this.transform(results1, max);
+    this.transform(results2, max);
+
+    return {data1: results1, data2: results2};
   },
 
   transform(data, max){
@@ -40,14 +44,12 @@ module.exports = {
       point.x = (600 / (data.length + 1)) * (idx + 1);
       point.y *= (590 / max);
     });
-  },
-
-  receiveTestCode(codeObj) {
-    //this is where code will be parsed
-    //array of lengths to test will come in - right now it will be come in as a const. [500, 1000, 2000, 4000, 8000, 16000, 32000]
-    //
-    let method1 = codeObj.algos.method1;
-    let method2 = codeObj.algos.method2;
-    VM.bootVM(method1, method2);
   }
 };
+
+// let method1 = eval(codeObj.algos.method1);
+// let method2 = eval(codeObj.algos.method2);
+// // let testN = [1000, 2000, 3000, 4000, 5000, 6000, 7000];
+// let testN = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+// let results1 = [];
+// let results2 = [];
