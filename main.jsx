c43f0a0fe5563db6c82706c0c3092acb63441889
DaC00P <dacuperman@gmail.com>
2016-08-03 18:21:48 -0700

    modal changes

--- main.jsx
+++ main.jsx
@@ -23,7 +23,8 @@ const customStyle = {
     left              : 0,
     right             : 0,
     bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    backgroundColor   : 'rgba(255, 255, 255, 0.75)',
+    zIndex            : 1000
   },
   content : {
     position                   : 'absolute',
