e1e7c8d5c9b2f0a0f842c8a24887a8e298a4c218
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-05 12:38:04 -0700

    modal styling changes

--- react/components/site_description_stepone.jsx
+++ react/components/site_description_stepone.jsx
@@ -4,7 +4,7 @@ const ReactDOM = require('react-dom');
 const SiteDescriptionOne = React.createClass({
   render(){
     return(
-      <div className='stepone-container'>
+      <div className='modal-container one'>
        <h2 className='welcome-text'>Welcome to Algo Arena!</h2>
 
        <p className='description-text'>Here at Algorithm Arena, we only care about one thing. How fast your array sorting functions are, and how good you are with plain old JavaScript!</p>
