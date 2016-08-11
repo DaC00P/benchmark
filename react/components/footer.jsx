c1d3e7309a99c55c1282352b9a72d3d4331f8877
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-11 15:55:56 -0700

    get rid of icons

--- react/components/footer.jsx
+++ react/components/footer.jsx
@@ -1,20 +1,16 @@
 const React = require('react');
-import FaIconPack from 'react-icons/lib/fa';
-import LD from 'react-icons/lib/fa/linkedin';
-import GH from 'react-icons/lib/fa/github-square';
-import EMAIL from 'react-icons/lib/ti/mail';
 
 const Footer = React.createClass({
 
   render: function() {
+    let text = "Site Designed & Built by Travis O'Neill & Daniel 'Coop' Cuperman"
     return (
       <div className="footer-column">
-          <p className='by-dc-to'>Site Designed & Built by Travis O'Neill & Daniel 'Coop' Cuperman</p>
+          <p className="by-dc-to">{text}</p>
           <ul className="connect-row">
             <strong className="heading">GITHUB</strong>
             <li className="git">
               <a className="icon-github-circled" href="https://github.com/travisoneill/benchmark" target='_blank'>
-                <GH id='icon'/>
               </a>
             </li>
           </ul>
