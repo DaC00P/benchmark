9068d593aad72c7d65d05414437baa8bb763d183
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-22 18:58:33 -0700

    returning results from node VM

--- api_gateway.py
+++ api_gateway.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, json
 
 ports = {
     'node': "8001",
@@ -31,31 +31,49 @@ def root(params):
 
 @app.route('/api/algos', methods=['POST'])
 def send():
-    data = request.json
-    servers = {}
+    incoming_data = request.json
+    test_params = incoming_data['lengthArr']
     responses = []
-    for service, server in services.items():
-        servers[server] = False
-
-    print('servers', servers)
-
-    for key, params in data.items():
-        if key[0:4] == 'data':
-            language = params['language']
-            server = services[language]
-            servers[server] = True
-
-    print('servers', servers)
-
-    for server, send_status in servers.items():
-        if send_status == True:
-            url = 'http://localhost:' + ports[server] + request.path
-            res = requests.post(url, data)
-            responses.append(res)
-            print(responses)
-
-    print(request.json['data1'])
-    return "REDIRECTED"
+    for key, request_data in incoming_data.items():
+        print(key, request_data)
+        if key[0:4] != 'data':
+            continue
+        language = request_data['language']
+        server = services[language]
+        request_url = 'http://localhost:' + ports[server] + request.path
+        outgoing_data = {'lengthArr': test_params, 'request_data': json.dumps(request_data)}
+        res = requests.post(request_url, outgoing_data)
+        responses.append(res)
+        print(responses)
+        return "DONE"
+        
+# @app.route('/api/algos', methods=['POST'])
+# def send():
+#     data = request.json
+#     servers = {}
+#     responses = []
+#     for service, server in services.items():
+#         servers[server] = False
+#
+#     print('servers', servers)
+#
+#     for key, params in data.items():
+#         if key[0:4] == 'data':
+#             language = params['language']
+#             server = services[language]
+#             servers[server] = True
+#
+#     print('servers', servers)
+#
+#     for server, send_status in servers.items():
+#         if send_status == True:
+#             url = 'http://localhost:' + ports[server] + request.path
+#             res = requests.post(url, data)
+#             responses.append(res)
+#             print(responses)
+#
+#     print(request.json['data1'])
+#     return "REDIRECTED"
 
 
 # def requestParser(params):
--- node/controller.js
+++ node/controller.js
@@ -2,38 +2,34 @@
 const VM = require('./vm');
 
 module.exports = {
+  // receiveCode(codeObj) {
+  //   let lengthArr = codeObj.lengthArr;
+  //   let method = codeObj.algos.method1;
+  //   let method2 = codeObj.algos.method2;
+  //   let name = codeObj.algos.name1;
+  //   let methodTwoName = codeObj.algos.name2;
+  //   return this.testCode(method1, methodOneName, method2, methodTwoName, lengthArr);
+  // },
   receiveCode(codeObj) {
-    let method1 = codeObj.algos.method1;
-    let method2 = codeObj.algos.method2;
-    let methodOneName = codeObj.algos.name1;
-    let methodTwoName = codeObj.algos.name2;
-    let lengthArr = codeObj.algos.lengthArr;
-    return this.testCode(method1, methodOneName, method2, methodTwoName, lengthArr);
+    console.log(codeObj)
+    const lengthArr = codeObj.lengthArr;
+    const requestData = JSON.parse(codeObj.request_data);
+    const method = requestData.method;
+    const name = requestData.name;
+    console.log([lengthArr, method, name]);
+    return this.testCode(method, name, lengthArr);
   },
 
-  testCode(method1, methodOneName, method2, methodTwoName, lengthArr) {
-    let results1 = [];
-    let results2 = [];
-    let res1;
-    let res2;
+  testCode(method, name, lengthArr) {
+    let results = [];
+    let res;
 
     for (let i = 0; i < lengthArr.length; i++) {
       try {
           let n = lengthArr[i];
-          let res1 = VM.bootVM(method1, methodOneName, n);
-          results1.push({x: n, y: res1});
-          if(res1 > 20000){ break; }
-      } catch(e) {
-          break;
-      }
-    }
-
-    for (let i = 0; i < lengthArr.length; i++) {
-      try {
-          let n = lengthArr[i];
-          let res2 = VM.bootVM(method2, methodTwoName, n);
-          results2.push({x: n, y: res2});
-          if(res2 > 20000){ break; }
+          let res = VM.bootVM(method, name, n);
+          results.push({x: n, y: res});
+          if(res > 20000){ break; }
       } catch(e) {
           break;
       }
@@ -41,10 +37,45 @@ module.exports = {
 
     return {
       xAxis: lengthArr,
-      rawData1: results1,
-      rawData2: results2,
-      name1: methodOneName,
-      name2: methodTwoName
+      rawData: results,
+      name: name,
     };
   },
+
+  // testCode(method1, name, method2, methodTwoName, lengthArr) {
+  //   let results1 = [];
+  //   let results2 = [];
+  //   let res1;
+  //   let res2;
+  //
+  //   for (let i = 0; i < lengthArr.length; i++) {
+  //     try {
+  //         let n = lengthArr[i];
+  //         let res1 = VM.bootVM(method1, name, n);
+  //         results1.push({x: n, y: res1});
+  //         if(res1 > 20000){ break; }
+  //     } catch(e) {
+  //         break;
+  //     }
+  //   }
+  //
+  //   for (let i = 0; i < lengthArr.length; i++) {
+  //     try {
+  //         let n = lengthArr[i];
+  //         let res2 = VM.bootVM(method2, methodTwoName, n);
+  //         results2.push({x: n, y: res2});
+  //         if(res2 > 20000){ break; }
+  //     } catch(e) {
+  //         break;
+  //     }
+  //   }
+  //
+  //   return {
+  //     xAxis: lengthArr,
+  //     rawData1: results1,
+  //     rawData2: results2,
+  //     name1: name,
+  //     name2: methodTwoName
+  //   };
+  // },
 };
