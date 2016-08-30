080a834f02c0b3c917a44a4a7d755b8b26707272
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-29 18:52:02 -0700

    ready

--- flask/flask_server.py
+++ flask/flask_server.py
@@ -9,20 +9,12 @@ def root():
 
 @app.route('/api/algos', methods=['POST'])
 def run():
-    # print(request.is_json)
     data = request.get_json(force=True)
     lengthArr = data['lengthArr']
     request_data = data['request_data']
     name = data['request_data']['name']
     result = benchmark.handle_request(lengthArr, request_data)
-    # print("RESULT")
-    # print(result)
-    print("JSON")
-    print(jsonify(result))
     response_data = {"xAxis": lengthArr, "name": name, "rawData": result}
-
-    # print(json.loads(data))
-    # print(json.dumps(data))
     return jsonify(response_data)
 
 if __name__ == "__main__":
--- node/node_server.js
+++ node/node_server.js
@@ -6,28 +6,9 @@ const Path = require('path');
 const Controller = require('./controller');
 const BodyParser = require('body-parser');
 
-
-// Server.use(Express.static(Path.join(__dirname + '/assets')));
-
-// Server.use(BodyParser.urlencoded({
-//   extended: true,
-//   parameterLimit: 10000,
-//   limit: 1024 * 1024 * 10
-// }));
-
 Server.use(BodyParser.json())
 
-// Server.get('/', function(req, res){
-//   res.sendFile(Path.join(__dirname + '/index.html'));
-// });
-
 Server.post('/api/algos', function(req, res){
-  // console.log("BODY");
-  // console.log(req.body);
-  // console.log("BODY");
-  // console.log(req.json);
-  // console.log(req.data);
-  // console.log(req);
   let data = Controller.receiveCode(req.body);
   console.log(data);
   res.send(data);
